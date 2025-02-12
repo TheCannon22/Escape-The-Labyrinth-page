83a536bad3a3ca38e37349ea8477cb29008ff7f4
Alexander Rankov <e2259198@cmaisonneuve.qc.ca>
2025-02-12 17:32:49 -0500

    J'ai ajouté un menu burger pour ma barre de navigation. De plus, j'ai fait un menu burger pour ma barre de navigation

--- src/composants/BarreDeNavigation.jsx
+++ src/composants/BarreDeNavigation.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './BarreDeNavigation.css';
 
 function BarreDeNavigation() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className='BarreDeNavigation'>
-            <ul>
+            <button className='menu-toggle' onClick={toggleMenu}>
+                ☰
+            </button>
+            <ul className={isOpen ? 'open' : ''}>
                 <li><Link to="/"> Accueil </Link> </li>
                 <li><Link to="/APropos"> À propos </Link> </li>
                 <li><Link to="/jouer"> Jouer </Link> </li>
